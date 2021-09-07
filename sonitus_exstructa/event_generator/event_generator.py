fe009af8d5da93d86766f76d081c7e927afa1e55
Nicholas Kiraly <kiraly.nicholas@gmail.com>
2021-09-06 22:17:59 -0400

    template generated messages, using specified word lists

--- sonitus_exstructa/event_generator/event_generator.py
+++ sonitus_exstructa/event_generator/event_generator.py
@@ -2,8 +2,13 @@
 Structured Logging Event Generator
 """
 
+import logging
+import random
+from string import Template
+from time import sleep
+
 DEFAULT_ACTORS = [
-    'Overlord'
+    'PowerManager'
 ]
 
 DEFAULT_VERBS = [
@@ -11,7 +16,7 @@ DEFAULT_VERBS = [
 ]
 
 DEFAULT_TARGETS = [
-    'pylons'
+    'accumulators'
 ]
 
 DEFAULT_STATES = [
@@ -36,8 +41,39 @@ class EventGenerator:
         self.states = states
         self.failures = failures
 
+        self._min_delay = 13
+        self._max_delay = 30
+
+        self._percent_failures = 10
+
+        self._success_template = Template("""$actor $verb $target $state""")
+        self._failure_template = Template("""$actor $failure $verb $target $state""")
+
     def generate(self):
         """
         Generate Log Messages Until Interrupted
         """
-        self.logger.info("Generate Message")
+        while True:
+            level = logging.INFO
+            msg = self._generate_message(self._success_template)
+
+            # is this message a failure
+            if random.randint(1, 100) <= self._percent_failures:
+                level = logging.ERROR
+                msg = self._generate_message(self._failure_template)
+
+            self.logger.log(
+                level,
+                msg,
+            )
+            sleep(random.randint(self._min_delay, self._max_delay))
+
+    def _generate_message(self, template):
+        msg = template.substitute(
+            actor=random.choice(self.actors),
+            verb=random.choice(self.verbs),
+            target=random.choice(self.targets),
+            state=random.choice(self.states),
+            failure=random.choice(self.failures),
+        )
+        return msg
